8c190698ddc252016abf8a59e684424485ffcb82
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-04 18:32:18 +0200

    add task 2-3 (con l'aiuto di Artur)

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,36 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+// Invoca la funzione qui e stampa il risultato in console
+console.log(first_word(names))
+
 
 // Dichiara la funzione qui.
+function first_word(array_names) {
+    const lettere = []
+
+    for (let i = 0; i < array_names.length; i++) {
+
+        lettere.push(array_names[i][0]);
+        // console.log(lettera)
+
+    }
+    return lettere;
+}
 
 
-// Invoca la funzione qui e stampa il risultato in console
+/* function iniziali(namesArray) {
+    const initials = [];
+
+    for (let i = 0; i < namesArray.length; i++) {
+        const name = namesArray[i];
+        initials.push(name.charAt(0));
+    }
+
+    return initials;
+}
 
 
+const initials = iniziali(names);
 
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log(initials);  */
--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -7,7 +7,43 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Invoca la funzione qui e stampa il risultato in console
+//1 Ciclo for della lunghezza dell'array
+//2 condizione IF per verificare se il nome inizia con "A"
+//3 se si, aggiungiamo ad un array vuoto quel nome
+//4 trasformiamo tutto in funzione
+
 
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+// const arrayvuoto = [];
+
+/* for (let i = 0; i < names.length; i++) {
+    /*  console.log(names[i])
+     console.log(names[i][0]) 
+
+    if (names[i][0] === firstLetter) {
+        // console.log("c'e un nome con la " + firstLetter) 
+        arrayvuoto.push(names[i])
+    }
+}
+console.log(arrayvuoto) */
+
+
+function wordWithA(names, firstLetter) {
+    const arrayvuoto = [];
+
+    for (let i = 0; i < names.length; i++) {
+        /*  console.log(names[i])
+         console.log(names[i][0]) */
+
+        if (names[i][0] === firstLetter) {
+            // console.log("c'e un nome con la " + firstLetter) 
+            arrayvuoto.push(names[i])
+        }
+    }
+    return arrayvuoto
+}
+
+console.log(wordWithA(names, 'A'))
