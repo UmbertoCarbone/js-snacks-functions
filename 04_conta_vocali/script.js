24f881eb0ce51adb415f79bdc7751a5b37272369
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-04 19:20:36 +0200

    add task 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,28 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
+// Dichiara la funzione qui.
+// Invoca la funzione qui e stampa il risultato in console
+//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+
+
 const word = 'javascript';
 
+console.log(estraiVocali(word));
 
-// Dichiara la funzione qui.
+function estraiVocali(vocal) {
+
+    const vocali = 'a,i';
+    let trovate = [];
+
+    for (let i = 0; i < vocal.length; i++) {
+        if (vocali.includes(vocal[i])) {
+            trovate.push(vocal[i]);
+        }
+    }
+    return trovate
+}
 
 
-// Invoca la funzione qui e stampa il risultato in console
 
-
-
-//Risultato atteso se si passa 'javascript': 3 (a, a, i)
